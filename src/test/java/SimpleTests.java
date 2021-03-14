366f69d830ec6378681df295a247de709952ffb1
nicedfx <nicedfx@gmail.com>
2021-03-14 22:26:02 +0300

    Add a negative test, add allure steps

--- src/test/java/SimpleTests.java
+++ src/test/java/SimpleTests.java
@@ -7,18 +7,38 @@ import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Selectors.byName;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
 
 public class SimpleTests extends TestBase{
 
     @Test
     void successTest() {
-        open("https://google.com");
-        $(byName("q")).setValue("Selenide").pressEnter();
-        $("#rcnt").shouldHave(Condition.text("selenide.org"));
+        step("Open google.com", () -> {
+            open("https://google.com");
+        });
+
+        step("search for 'selenide'", () -> {
+            $(byName("q")).setValue("Selenide").pressEnter();
+        });
+        step("Check that selenide is displayed in the search results'", () -> {
+            $("#rcnt").shouldHave(Condition.text("selenide.org"));
+        });
+
+
+
 
     }
     @Test
     void negativeTest() {
+        step("Open google.com", () -> {
+            open("https://google.com");
+        });
 
+        step("search for 'selenide'", () -> {
+            $(byName("q")).setValue("Selenide").pressEnter();
+        });
+        step("Check that weather.com is displayed in the search results'", () -> {
+            $("#rcnt").shouldHave(Condition.text("weather.com"));
+        });
     }
 }
