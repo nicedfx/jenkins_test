0337913c6ea9a8c23cfa3ef609d2a6742b007194
nicedfx <nicedfx@gmail.com>
2021-03-16 10:41:34 +0300

    Add aftereach method with attachments

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,10 +1,12 @@
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.Browsers.CHROME;
+import static helpers.AttachmentsHelper.*;
 
 public class TestBase {
 
@@ -42,5 +44,11 @@ public class TestBase {
 //                URI.create("http://selenoid:4444/wd/hub").toURL(),
 //                capabilities
 //        );
+    }
+    @AfterEach
+    public void afterEach() {
+        attachScreenshot("Last screenshot");
+        attachPageSource();
+        attachAsText("Browser console logs", getConsoleLogs());
     }
 }
