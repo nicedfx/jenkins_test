0f3891197cc7eae11b00b86d465c78bab2cc83ec
nicedfx <nicedfx@gmail.com>
2021-03-18 16:24:01 +0300

    Add Owner parametrization

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,6 +1,7 @@
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -14,37 +15,21 @@ public class TestBase {
     public static void setUp() {
         SelenideLogger.addListener("allure", new AllureSelenide());
 
+        TestConfig config = ConfigFactory.create(TestConfig.class, System.getProperties());
+
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
         Configuration.browserCapabilities = capabilities;
-//        Configuration.browserVersion="85.0";
-        Configuration.browser = System.getProperty("browser", "firefox");
-        Configuration.startMaximized = true;
-//        Configuration.remote = "http://user1:Z^p&F@selenoid.sbacho.ml:4444/wd/hub";
-        System.out.println("http" + System.getProperty("selenoid") + ":4444/wd/hub");
-        Configuration.remote = "http" + System.getProperty("selenoid") + ":4444/wd/hub";
+        Configuration.browserVersion= config.browserVersion();
+        Configuration.browser = config.browser();
+        Configuration.startMaximized = config.startMaximized();
 
-        // config for Java + Selenide
-//        DesiredCapabilities capabilities = new DesiredCapabilities();
-//        capabilities.setCapability("enableVNC", true);
-//        capabilities.setCapability("enableVideo", true);
-//        Configuration.browserCapabilities = capabilities;
-//        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud:4444/wd/hub/";
-
-        // config for Java + Selenium
-//        DesiredCapabilities capabilities = new DesiredCapabilities();
-//        capabilities.setCapability("browserName", "chrome");
-//        capabilities.setCapability("browserVersion", "87.0");
-//        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
-//                "enableVNC", true,
-//                "enableVideo", true
-//        ));
-//        RemoteWebDriver driver = new RemoteWebDriver(
-//                URI.create("http://selenoid:4444/wd/hub").toURL(),
-//                capabilities
-//        );
+        if (config.isRemote() == true) {
+            Configuration.remote = config.selenoidUrl();
+        }
     }
+
     @AfterEach
     public void afterEach() {
         attachScreenshot("Last screenshot");
